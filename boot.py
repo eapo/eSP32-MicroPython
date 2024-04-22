9f09e0bc72f00cf299945760f3749c804ac5993e
eapo <e@eaposztrof.com>
2024-04-22 03:49:03 +0300

    beautify & colorize terminal

--- boot.py
+++ boot.py
@@ -1,4 +1,4 @@
-print('\ninit boot.py\n...')
+print('\n\033[1;32m' + 'init boot.py' + '\033[0m\n\033[5m...\033[0m')
 
 import config  # Import configuration file
 import network
@@ -16,9 +16,9 @@ wifi.connect(wifi_ssid, wifi_password)
 while not wifi.isconnected():
     time.sleep(1)
 
-print("Connected to Wi-Fi:", wifi_ssid, wifi.ifconfig())
-
+print("\033[32mConnected to Wi-Fi:\033[0m", wifi_ssid, wifi.ifconfig())
+print("\033[32m")
 import webrepl
 webrepl.start()
 
-print("WebREPL started")
+print("WebREPL started\033[0m")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print('\ninit main.py\n...')
+print('\n\033[1;32m' + 'init main.py' + '\033[0m\n\033[5m...\033[0m')
 
 import time
 import machine
@@ -14,21 +14,17 @@ hex_unique_id = binascii.hexlify(unique_id).decode('utf-8')
 
 celsius = (esp32.raw_temperature() - 32) * 5.0 / 9.0
 
-print("machine.reset_cause:",machine.reset_cause())
-print("machine.unique_id:",machine.unique_id(),"|",hex_unique_id)
-print("machine.freq:",machine.freq()/1000000,"MHz")
-print("esp32.raw_temperature:",celsius,"C")
+print("\033[32mmachine.reset_cause:\033[0m\t",machine.reset_cause())
+print("\033[32mmachine.unique_id:\033[0m\t",machine.unique_id(),"\033[32m|\033[0m",hex_unique_id)
+print("\033[32mmachine.freq:\033[0m\t\t",machine.freq()/1000000,"MHz")
+print("\033[32mesp32.raw_temperature:\033[0m\t",celsius,"C")
 
 lsroot = os.listdir('/')
-print("os.listdir('/'):")
+print("\033[32mos.listdir('/'):\033[0m\t",len(lsroot),"items")
 for item in lsroot:
     if (os.stat(item)[0] == 16384):
-        print("\t","/" + item + "/")
+        print(" 📁","/\033[0;97m" + item + "\033[0m/")
     else:
-        print("\t","/" + item,str(os.stat(item)[6])+"b")
+        print(" 📄","/" + item,str(os.stat(item)[6]) + "b")
 
-print('\nHello world!')
-
-
-
-
+print('\n\033[1;97m🖖🤖🐍\033[0m\n')
