fdd4b8f3fc10cfe742a599ef48c054547147e2ef
eapo <e@eaposztrof.com>
2024-04-24 09:33:19 +0300

    init test()

--- hw/ws2815.py
+++ hw/ws2815.py
@@ -20,35 +20,38 @@ def set_color(r, g, b):
     np[i] = (r, g, b)
   np.write()
 
-print('\n\033[1;32m' + 'RGB order testing' + '\033[0m\n\033[5m🚥🚥🚥\033[0m')
-for i in range(n):
-    np[i-1] = (i, 0, 0)
-    np.write()
-    time.sleep(0.01)
-for i in range(n):
-    np[i-1] = (0, i, 0)
-    np.write()
-    time.sleep(0.01)
-for i in range(n):
-    np[i-1] = (0, 0, i)
-    np.write()
-    time.sleep(0.01)
-for i in range(n):
-    np[i-1] = (0, 0, 0)
-    np.write()
-    time.sleep(0.01)
-    
-for i in range(3):
-    clear()
-    time.sleep(0.01)
-    np[0] = (10, 10, 10)
-    np[1] = (10, 0, 0)
-    np[2] = (0, 10, 0)
-    np[3] = (0, 0, 10)
-    np[n-1] = (255, 255, 255)
-    np[n-2] = (255, 255, 255)
-    np[n-3] = (255, 255, 255)
-    np.write()
-    time.sleep(0.01)
+def init_test():
+  print('\n\033[1;32m' + 'RGB order testing' + '\033[0m\n\033[5m🚥🚥🚥\033[0m')
+  for i in range(n):
+      np[i-1] = (i, 0, 0)
+      np.write()
+      time.sleep(0.01)
+  for i in range(n):
+      np[i-1] = (0, i, 0)
+      np.write()
+      time.sleep(0.01)
+  for i in range(n):
+      np[i-1] = (0, 0, i)
+      np.write()
+      time.sleep(0.01)
+  for i in range(n):
+      np[i-1] = (0, 0, 0)
+      np.write()
+      time.sleep(0.01)
+      
+  for i in range(3):
+      clear()
+      time.sleep(0.01)
+      np[0] = (10, 10, 10)
+      np[1] = (10, 0, 0)
+      np[2] = (0, 10, 0)
+      np[3] = (0, 0, 10)
+      np[n-1] = (255, 255, 255)
+      np[n-2] = (255, 255, 255)
+      np[n-3] = (255, 255, 255)
+      np.write()
+      time.sleep(0.01)
+
+init_test()
 
 print('\n🚥🚥🚥✅\n')
